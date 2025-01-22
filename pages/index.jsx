e90e3924684c0b5b02227cde5f4d9c56ed36d238
Dongmin <emthrology@gmail.com>
2025-01-22 15:32:41 +0900

    컴포넌트화

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,28 +1,11 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import ProductList from '@/components/product/ProductList';
 
 function ProductPage() {
-  //페이지에서 state로 샤용할 객체 (setter 필수)
-  const [products, setProducts] = useState();
-  const [test, setTest] = useState(); //이런 식으로 원하는 객체를 추가로 설정 가능
-
-  //컴포넌트 렌더링 되자마자 실행될 로직들
-  useEffect(() => {
-    axios.get('http://localhost:4000/products').then(response => {
-      setProducts(response.data);
-    });
-  }, []);
-  console.log(products);
   return (
-    <div style={{ padding: '10px' }}>
+    <>
       <h1>상품목록</h1>
-      <ul>
-        {products &&
-          products.map(product => {
-            return <li key={product.id}>{product.name}</li>;
-          })}
-      </ul>
-    </div>
+      <ProductList />
+    </>
   );
 }
 
