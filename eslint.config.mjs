beb463e4194b2628b0b5372a7e710cdfa62a231a
Dongmin <emthrology@gmail.com>
2025-02-14 15:39:53 +0900

    cypress setting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 import prettierPlugin from "eslint-plugin-prettier";
+import cypress from "cypress";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,11 +14,13 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals"),
   ...compat.extends("plugin:prettier/recommended"),
+  ...compat.extends("plugin:cypress/recommended"),
 
   {
     files: ["**/*.js", "**/*.jsx"],
     plugins: {
       prettier: prettierPlugin,
+      cypress: cypress,
     },
     rules: {
       "no-undef": "error",
